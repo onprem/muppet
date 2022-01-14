3dce1442e35dc2a17887aa5d07e6b5507bdc13b3
Prem Saraswat <prmsrswt@gmail.com>
2022-01-14 08:35:45 +0530

    Add flags and logging to agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,22 +2,53 @@ package main
 
 import (
 	"context"
-	"log"
+	"errors"
 	"os"
 	"os/exec"
 	"time"
 
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/oklog/run"
+	flag "github.com/spf13/pflag"
+
 	"github.com/onprem/muppet/pkg/api"
 )
 
-func main() {
-	client, err := api.NewClientWithResponses("http://localhost:8080")
-	if err != nil {
-		log.Fatal(err)
+type config struct {
+	serviceURL string
+	hostname   string
+}
+
+func parseFlags() (*config, error) {
+	cfg := &config{}
+
+	flag.StringVar(&cfg.serviceURL, "service-url", "http://localhost:8080", "The URL of muppet service to fetch commands from.")
+	flag.StringVar(&cfg.hostname, "hostname", "", "The hostname to fetch commands for.")
+
+	flag.Parse()
+
+	if cfg.hostname == "" {
+		return nil, errors.New("hostname is required")
 	}
 
-	hostname := "host001"
+	return cfg, nil
+}
+
+func main() {
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+
+	cfg, err := parseFlags()
+	if err != nil {
+		level.Error(logger).Log("msg", "parsing flags", "err", err)
+		return
+	}
+
+	client, err := api.NewClientWithResponses(cfg.serviceURL)
+	if err != nil {
+		level.Error(logger).Log("msg", "creating API client", "err", err)
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 
@@ -27,35 +58,40 @@ func main() {
 
 	{
 		fn := func(ctx context.Context) error {
-			resp, err := client.ListCommandQueueWithResponse(ctx, hostname)
+			resp, err := client.ListCommandQueueWithResponse(ctx, cfg.hostname)
 			if err != nil {
 				return err
 			}
 
 			for _, v := range *resp.JSON200 {
+				logger = log.With(logger, "uuid", v.Uuid, "command", v.ShellCommand)
+
 				cmd := exec.CommandContext(ctx, "sh", "-c", v.ShellCommand)
 				_ = cmd.Run()
-				log.Printf("ran commad: %s; exit status: %d\n", v.ShellCommand, cmd.ProcessState.ExitCode())
+
+				level.Info(logger).Log("msg", "ran command", "exitcode", cmd.ProcessState.ExitCode())
+
 				_, err := client.MarkCommandDone(
 					ctx,
-					hostname,
+					cfg.hostname,
 					api.MarkCommandDoneJSONRequestBody{Uuid: v.Uuid, ExitStatus: float32(cmd.ProcessState.ExitCode())},
 				)
 
 				if err != nil {
-					log.Printf("err: %v", err)
+					level.Error(logger).Log("msg", "marking command as done", "err", err)
 				}
 			}
 
 			return nil
 		}
+
 		g.Add(func() error {
 			ticker := time.NewTicker(time.Minute)
 			for {
 				select {
 				case <-ticker.C:
 					if err := fn(ctx); err != nil {
-						log.Printf("err: %v", err)
+						level.Error(logger).Log("msg", "fetch and run commands", "err", err)
 					}
 				case <-ctx.Done():
 					return nil
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/goleak v1.1.10 // indirect
 	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
 	golang.org/x/tools v0.0.0-20210114065538-d78b04bdf963 // indirect
